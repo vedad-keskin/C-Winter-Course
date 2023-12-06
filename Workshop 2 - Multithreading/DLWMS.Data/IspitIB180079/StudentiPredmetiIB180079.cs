bde80b6cbd0e82300d075f0d53132c0976e17b6d
Vedad <vedad.ribery@gmail.com>
2023-12-06 03:35:33 +0100

    Upload druge radionice
    Radjen multithreading

---
+++ Workshop 2 - Multithreading/DLWMS.Data/IspitIB180079/StudentiPredmetiIB180079.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLWMS.Data.IspitIB180079
+{
+    public class StudentiPredmetiIB180079
+    {
+        public int Id { get; set; }
+        public int StudentId { get; set; }
+        public Student Student { get; set; } // ime prezime indeks
+
+        public int PredmetId { get; set; }
+        public PredmetiIB180079 Predmet { get; set; }
+        public int Ocjena { get; set; }
+        public DateTime Datum { get; set; }
+
+
+
+    }
+}

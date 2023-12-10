93b23224e86d18040b0527e0725e9208c2dbfa2b
Vedad <vedad.ribery@gmail.com>
2023-12-10 15:27:21 +0100

    Upload trece radionice

---
+++ Workshop 3 - Reporting/New Template/DLWMS.Data/IspitIB170074/StudentiPredmetiIB170074.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DLWMS.Data.IspitIB170074
+{
+    public class StudentiPredmetiIB170074
+    {
+        public int Id { get; set; }
+        public int StudentId { get; set; }
+        public Student Student { get; set; }
+        public int PredmetId { get; set; }
+        public PredmetiIB170074 Predmet { get; set; }
+        public int Ocjena { get; set; }
+        public DateTime Datum { get; set; }
+    }
+}
